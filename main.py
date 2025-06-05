ef7db62cfce9eb69bc2f8b8dab727d29500e3f9e
hamdey3223 <hamdyahmed01221@gmail.com>
2025-06-06 02:09:55 +0300

    4 comment

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from datetime import datetime
 import csv
 from io import BytesIO
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "Y8058352424:AAHSOnxnsNQXXNfh4mLHRrZv5ca3LojWiOs"
 ADMIN_ID = 123456789  # حط الآي دي بتاعك هنا
 WHATSAPP_NUMBER = "201095587980"
 
